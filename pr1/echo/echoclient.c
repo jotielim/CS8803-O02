75f050f1484901add04dfbf185922f3e430d6bec
Yonathan Lim <ylim@theranos.com>
2016-06-07 00:42:18 -0700

    Update echoclient and echoserver

--- pr1/echo/echoclient.c
+++ pr1/echo/echoclient.c
@@ -9,7 +9,9 @@
 #include <getopt.h>
 
 /* Be prepared accept a response of this length */
-#define BUFSIZE 4096
+//#define BUFSIZE 4096
+#define BUFSIZE 16
+#define MAXSTRING (BUFSIZE-1)
 
 #define USAGE                                                                 \
 "usage:\n"                                                                    \
@@ -18,37 +20,93 @@
 "  -s                  Server (Default: localhost)\n"                         \
 "  -p                  Port (Default: 8888)\n"                                \
 "  -m                  Message to send to server (Default: \"Hello World!\"\n"\
-"  -h                  Show this help message\n"                              
+"  -h                  Show this help message\n"
 
 /* Main ========================================================= */
 int main(int argc, char **argv) {
-	int option_char = 0;
-	char *hostname = "localhost";
-	unsigned short portno = 8888;
-	char *message = "Hello World!";
+    int option_char = 0;
+    char *hostname = "localhost";
+    unsigned short portno = 8888;
+    char *message = "Hello World!";
 
-	// Parse and set command line arguments
-	while ((option_char = getopt(argc, argv, "s:p:m:h")) != -1) {
-		switch (option_char) {
-			case 's': // server
-				hostname = optarg;
-				break; 
-			case 'p': // listen-port
-				portno = atoi(optarg);
-				break;                                        
-			case 'm': // server
-				message = optarg;
-				break;    
-			case 'h': // help
-				fprintf(stdout, "%s", USAGE);
-				exit(0);
-				break;       
-			default:
-				fprintf(stderr, "%s", USAGE);
-				exit(1);
-		}
-	}
+    // Parse and set command line arguments
+    while ((option_char = getopt(argc, argv, "s:p:m:h")) != -1) {
+        switch (option_char) {
+            case 's': // server
+                hostname = optarg;
+                break;
+            case 'p': // listen-port
+                portno = atoi(optarg);
+                break;
+            case 'm': // server
+                message = optarg;
+                break;
+            case 'h': // help
+                fprintf(stdout, "%s", USAGE);
+                exit(0);
+                break;
+            default:
+                fprintf(stderr, "%s", USAGE);
+                exit(1);
+        }
+    }
 
-	/* Socket Code Here */
+    /* Socket Code Here */
 
+    // 1. Create a socket with the socket() system call
+    // 2. Connect the socket to the address of the server using the connect() system call
+    // 3. Send and receive data. There are a number of ways to do this, but the simplest is to use
+    //    the read() and write() system calls.
+
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    struct hostent *he;
+
+    char buffer[BUFSIZE];
+    char recvline[MAXSTRING];
+
+    // clean the buffer
+    memset(&buffer, 0, BUFSIZE);
+
+    // create the socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Unable to create the socket");
+        exit(1);
+    }
+
+    // TODO: use getaddrinfo instead of gethostbyname
+    // resolve hostname
+    if ((he = gethostbyname(hostname)) == NULL) {
+        perror("Hostname not found");
+        close(sockfd);
+        exit(2); // exit when error or hostname is not found
+    }
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    // copy the network address to sockaddr_in structure
+    memcpy(&serv_addr.sin_addr, he->h_addr_list[0], he->h_length);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+
+    // connect the client to the socket
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) {
+        perror("Unable to connect to the server");
+        close(sockfd);
+        exit(3); // error
+    }
+
+    // send message to the server
+    if (send(sockfd, message, strlen(message), 0) < 0) {
+        perror("Unable to send message to the server");
+        close(sockfd);
+        exit(4);
+    }
+
+    // receive message from the server
+    recv(sockfd, recvline, MAXSTRING, 0);
+    fputs(recvline, stdout);
+
+    // close the socket before exiting
+    close(sockfd);
+    exit(0);
 }
--- pr1/echo/echoserver.c
+++ pr1/echo/echoserver.c
@@ -3,19 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #if 0
-/* 
+/*
  * Structs exported from netinet/in.h (for easy reference)
  */
 
 /* Internet address */
 struct in_addr {
-  unsigned int s_addr; 
+  unsigned int s_addr;
 };
 
 /* Internet style socket address */
@@ -40,7 +41,9 @@ struct hostent {
 }
 #endif
 
-#define BUFSIZE 4096
+//#define BUFSIZE 4096
+#define BUFSIZE 16
+#define MAXSTRING (BUFSIZE-1)
 
 #define USAGE                                                                 \
 "usage:\n"                                                                    \
@@ -48,32 +51,84 @@ struct hostent {
 "options:\n"                                                                  \
 "  -p                  Port (Default: 8888)\n"                                \
 "  -n                  Maximum pending connections\n"                         \
-"  -h                  Show this help message\n"                              
+"  -h                  Show this help message\n"
 
 int main(int argc, char **argv) {
-  int option_char;
-  int portno = 8888; /* port to listen on */
-  int maxnpending = 5;
-  
-  // Parse and set command line arguments
-  while ((option_char = getopt(argc, argv, "p:n:h")) != -1){
-    switch (option_char) {
-      case 'p': // listen-port
-        portno = atoi(optarg);
-        break;                                        
-      case 'n': // server
-        maxnpending = atoi(optarg);
-        break; 
-      case 'h': // help
-        fprintf(stdout, "%s", USAGE);
-        exit(0);
-        break;           
-      default:
-        fprintf(stderr, "%s", USAGE);
-        exit(1);
+    int option_char;
+    int portno = 8888; /* port to listen on */
+    int maxnpending = 5;
+
+    // Parse and set command line arguments
+    while ((option_char = getopt(argc, argv, "p:n:h")) != -1){
+        switch (option_char) {
+            case 'p': // listen-port
+                portno = atoi(optarg);
+                break;
+            case 'n': // server
+                maxnpending = atoi(optarg);
+                break;
+            case 'h': // help
+                fprintf(stdout, "%s", USAGE);
+                exit(0);
+                break;
+            default:
+                fprintf(stderr, "%s", USAGE);
+                exit(1);
+        }
     }
-  }
 
-  /* Socket Code Here */
+    /* Socket Code Here */
 
+    // 1. Create a socket with the socket() system call.
+    // 2. Bind the socket to an address using the bind() system call. For a server socket on the Internet,
+    //    an address consists of a port number on the host machine.
+    // 3. Listen for connections with the listen() system call.
+    // 4. Accept a connection with the accept() system call. This call typically blocks until a client connects
+    //    with the server.
+    // 5. Send and receive data.
+
+    int listenfd, connfd, n;
+    struct sockaddr_in serv_addr;
+
+    char buffer[BUFSIZE];
+
+    // clean the buffer
+    memset(&buffer, 0, BUFSIZE);
+
+    // create socket
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // prepare the sockaddr_in structure
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    serv_addr.sin_port = htons(portno);
+
+    // bind the socket to the address
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    // listen to maximum pending connections
+    listen(listenfd, maxnpending);
+
+    // infinite loop, continuously listening
+    for ( ; ; ) {
+        // accept connection from an incoming client
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+
+        if ((n = recv(connfd, buffer, MAXSTRING, 0)) > 0) {
+            // echo back to the client
+            send(connfd, buffer, n, 0);
+        }
+
+        if (n < 0) {
+            perror("Read error");
+            exit(1);
+        }
+
+        // close the accepted connection
+        close(connfd);
+    }
+
+    // close listening socket
+    close(listenfd);
 }
