a4630f72b534acecd66319f2769e05b9d329dfff
Yonathan Lim <ylim@theranos.com>
2016-06-08 00:11:37 -0700

    Complete transfer component

--- pr1/transfer/transferclient.c
+++ pr1/transfer/transferclient.c
@@ -17,37 +17,117 @@
 "  -s                  Server (Default: localhost)\n"                         \
 "  -p                  Port (Default: 8888)\n"                                \
 "  -o                  Output file (Default foo.txt)\n"                       \
-"  -h                  Show this help message\n"                              
+"  -h                  Show this help message\n"
+
+// declare header for function recv_file
+int recv_file(int sockfd, char *filename);
 
 /* Main ========================================================= */
 int main(int argc, char **argv) {
-	int option_char = 0;
-	char *hostname = "localhost";
-	unsigned short portno = 8888;
-	char *filename = "foo.txt";
+    int option_char = 0;
+    char *hostname = "localhost";
+    unsigned short portno = 8888;
+    char *filename = "foo.txt";
 
-	// Parse and set command line arguments
-	while ((option_char = getopt(argc, argv, "s:p:o:h")) != -1) {
-		switch (option_char) {
-			case 's': // server
-				hostname = optarg;
-				break; 
-			case 'p': // listen-port
-				portno = atoi(optarg);
-				break;                                        
-			case 'o': // filename
-				filename = optarg;
-				break;       
-			case 'h': // help
-				fprintf(stdout, "%s", USAGE);
-				exit(0);
-				break;       
-			default:
-				fprintf(stderr, "%s", USAGE);
-				exit(1);
-		}
-	}
+    // Parse and set command line arguments
+    while ((option_char = getopt(argc, argv, "s:p:o:h")) != -1) {
+        switch (option_char) {
+            case 's': // server
+                hostname = optarg;
+                break;
+            case 'p': // listen-port
+                portno = atoi(optarg);
+                break;
+            case 'o': // filename
+                filename = optarg;
+                break;
+            case 'h': // help
+                fprintf(stdout, "%s", USAGE);
+                exit(0);
+                break;
+            default:
+                fprintf(stderr, "%s", USAGE);
+                exit(1);
+        }
+    }
 
-	/* Socket Code Here */
+    /* Socket Code Here */
 
+    // 1. Create a socket with the socket() system call
+    // 2. Connect the socket to the address of the server using the connect() system call
+    // 3. Send and receive data.
+
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    struct hostent *he;
+
+    char buffer[BUFSIZE];
+    char recvline[BUFSIZE];
+
+    // clean the buffer
+    memset(&buffer, 0, BUFSIZE);
+
+    // create the socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Unable to create the socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // resolve hostname
+    if ((he = gethostbyname(hostname)) == NULL) {
+        perror("Hostname not found");
+        close(sockfd);
+        exit(EXIT_FAILURE); // exit when error or hostname is not found
+    }
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    // copy the network address to sockaddr_in structure
+    memcpy(&serv_addr.sin_addr, he->h_addr_list[0], he->h_length);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+
+    // connect the client to the socket
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) {
+        perror("Unable to connect to the server");
+        close(sockfd);
+        exit(EXIT_FAILURE); // error
+    }
+
+    // get bytes from the server
+    recv_file(sockfd, filename);
+
+    // close the socket before exiting
+    close(sockfd);
+    exit(EXIT_SUCCESS);
+}
+
+int recv_file(int sockfd, char *filename) {
+    int f, recv_count, recv_size;
+    ssize_t recv_bytes;
+    char buffer[BUFSIZE];
+
+    // create file to attempt to save received data
+    if ((f = open(filename, O_WRONLY|O_CREAT, 0644)) < 0) {
+        perror("error creating file");
+        return -1;
+    }
+
+    // reset counters to 0
+    recv_count = 0;
+    recv_size = 0;
+
+    // receive file from server
+    while ((recv_bytes = recv(sockfd, buffer, BUFSIZE, 0)) > 0) {
+        recv_count++;
+        recv_size += recv_bytes;
+
+        // save the received buffer data to local file
+        // terminate when error occurs
+        if (write(f, buffer, (size_t)recv_bytes) < 0) {
+            perror("error writing to file");
+            return -1;
+        }
+    }
+    close(f); // close file
+    printf("Client received %d bytes in %d recv(s)\n", recv_size, recv_count);
+    return recv_size;
 }
--- pr1/transfer/transferserver.c
+++ pr1/transfer/transferserver.c
@@ -3,20 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 
 #if 0
-/* 
+/*
  * Structs exported from netinet/in.h (for easy reference)
  */
 
 /* Internet address */
 struct in_addr {
-  unsigned int s_addr; 
+  unsigned int s_addr;
 };
 
 /* Internet style socket address */
@@ -49,32 +49,116 @@ struct hostent {
 "options:\n"                                                                  \
 "  -p                  Port (Default: 8888)\n"                                \
 "  -f                  Filename (Default: bar.txt)\n"                         \
-"  -h                  Show this help message\n"                              
+"  -h                  Show this help message\n"
+
+// declare header for function send_file
+int send_file(int connfd, char *filename);
 
 int main(int argc, char **argv) {
-  int option_char;
-  int portno = 8888; /* port to listen on */
-  char *filename = "bar.txt"; /* file to transfer */
+    int option_char;
+    int portno = 8888; /* port to listen on */
+    char *filename = "bar.txt"; /* file to transfer */
+    int maxnpending = 5;
 
-  // Parse and set command line arguments
-  while ((option_char = getopt(argc, argv, "p:f:h")) != -1){
-    switch (option_char) {
-      case 'p': // listen-port
-        portno = atoi(optarg);
-        break;                                        
-      case 'f': // listen-port
-        filename = optarg;
-        break;   
-      case 'h': // help
-        fprintf(stdout, "%s", USAGE);
-        exit(0);
-        break;       
-      default:
-        fprintf(stderr, "%s", USAGE);
-        exit(1);
+    // Parse and set command line arguments
+    while ((option_char = getopt(argc, argv, "p:f:h")) != -1){
+        switch (option_char) {
+            case 'p': // listen-port
+                portno = atoi(optarg);
+                break;
+            case 'n': // server
+                maxnpending = atoi(optarg);
+                break;
+            case 'f': // filename
+                filename = optarg;
+                break;
+            case 'h': // help
+                fprintf(stdout, "%s", USAGE);
+                exit(0);
+                break;
+            default:
+                fprintf(stderr, "%s", USAGE);
+                exit(1);
+        }
     }
-  }
 
-  /* Socket Code Here */
+    /* Socket Code Here */
 
+    // 1. Create a socket with the socket() system call.
+    // 2. Bind the socket to an address using the bind() system call. For a server socket on the Internet,
+    //    an address consists of a port number on the host machine.
+    // 3. Listen for connections with the listen() system call.
+    // 4. Accept a connection with the accept() system call. This call typically blocks until a client connects
+    //    with the server.
+    // 5. Send and receive data.
+
+    int listenfd, connfd, n;
+    struct sockaddr_in serv_addr;
+
+    char buffer[BUFSIZE];
+
+    // clean the buffer
+    memset(&buffer, 0, BUFSIZE);
+
+    // create socket
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // prepare the sockaddr_in structure
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htons(INADDR_ANY);
+    serv_addr.sin_port = htons(portno);
+
+    // bind the socket to the address
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    // listen to maximum pending connections
+    listen(listenfd, maxnpending);
+
+    // infinite loop, continuously listening
+    for ( ; ; ) {
+        // accept connection from an incoming client
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+
+        // send the file
+        if (send_file(connfd, filename) < 0) {
+            perror("fail sending file");
+            exit(EXIT_FAILURE);
+        }
+
+        // close the accepted connection
+        close(connfd);
+    }
+}
+
+int send_file(int connfd, char *filename) {
+    int send_count;
+    ssize_t read_bytes, send_bytes, send_size;
+    char buffer[BUFSIZE];
+    int f;
+
+    // reset counters to 0
+    send_count = 0;
+    send_size = 0;
+
+    // open the file as read only
+    // if file not found, terminate the program
+    if ((f = open(filename, O_RDONLY)) < 0) {
+        perror(filename);
+        return -1;
+    }
+
+    // loop until there are no more bytes left to read
+    while ((read_bytes = read(f, buffer, BUFSIZE)) > 0) {
+        // send bytes to the client
+        // terminate when error occur
+        if ((send_bytes = send(connfd, buffer, (size_t)read_bytes, 0)) < read_bytes) {
+            perror("send error");
+            return  -1;
+        }
+        send_count++;
+        send_size += send_bytes;
+    }
+    close(f); // close file
+    return send_count;
 }
