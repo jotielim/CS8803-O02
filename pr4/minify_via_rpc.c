b887ce13813a3a6fc09534d591b729a4f8b62263
Yonathan Lim <ylim@theranos.com>
2016-07-24 16:43:39 -0700

    pr4: change timeout to 5 second and add handler for timed out rpc call

--- pr4/minify_via_rpc.c
+++ pr4/minify_via_rpc.c
@@ -5,14 +5,20 @@
 
 CLIENT* get_minify_client(char *server){
     CLIENT *cl;
+    struct timeval tv;
 
     /* Your code here */
+
     // establish tcp connection
     cl = clnt_create(server, MINIFYJPEG_PROG, MINIFYJPEG_VERS, "tcp");
     if (cl == NULL) {
         clnt_pcreateerror(server);
         exit(1);
     }
+
+    tv.tv_sec = 5; // second
+    tv.tv_usec = 0; // microsecond
+    clnt_control(cl, CLSET_TIMEOUT, &tv);
 
     return cl;
 }
@@ -21,6 +27,7 @@ CLIENT* get_minify_client(char *server){
 void* minify_via_rpc(CLIENT *cl, void* src_val, size_t src_len, size_t *dst_len){
 
 	/*Your code here */
+
     image_t minifyjpeg_1_arg;
     // allocate memory for the result
     image_t *result = malloc(sizeof(*result));
@@ -32,14 +39,22 @@ void* minify_via_rpc(CLIENT *cl, void* src_val, size_t src_len, size_t *dst_len)
     // RPC call to minify image passing result pointer as argument;
     rpc_response = minifyjpeg_proc_1(minifyjpeg_1_arg, result, cl);
 
-    if (rpc_response != RPC_SUCCESS) {
-        clnt_perror(cl, "call failed");
-        return NULL;
+    // exit when timed out
+    if (rpc_response == RPC_TIMEDOUT) {
+        clnt_perror(cl, "timeout");
+        exit(-1);
     }
 
+    // exist if not RPC_SUCCESS
+    if (rpc_response != RPC_SUCCESS) {
+        clnt_perror(cl, "call failed");
+        exit(-1);
+    }
+
+    // exit if result i NULL
     if (result == NULL) {
         clnt_perror(cl, "result is null");
-        return NULL;
+        exit(-1);
     }
 
     // assign the returned image length to dst_len pointer;
